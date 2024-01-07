3e685f3767b520dde0d11d343d0125d32f8d6f66
BO <@>
2024-01-07 20:20:56 +0800

    初始

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use std::{env, fs, process};
+use std::error::Error;
+
+#[derive(Debug, PartialEq)]
+pub struct 输入配置 {
+    pub 查询字段: Option<String>,
+    pub 文件地址: String,
+    打印全部吗: bool,
+}
+
+impl 输入配置 {
+    pub fn 获取配置() -> Result<输入配置, String> {
+        let 命令行参数: Vec<String> = env::args().skip(1).collect();
+        if 命令行参数.len() < 2 { return Err(format!("命令行参数过少 :{:?}", 命令行参数)); }
+        if 命令行参数.len() > 2 { return Err(format!("命令行参数过多 :{:?}", 命令行参数)); }
+        if 命令行参数[1] == "-all" { return Ok(输入配置 { 查询字段: None, 文件地址: 命令行参数[0].clone(), 打印全部吗: true }); }
+        let 查询字段 = 命令行参数[0].clone();
+        let 文件地址 = 命令行参数[1].clone();
+        Ok(输入配置 { 查询字段: Some(查询字段), 文件地址, 打印全部吗: false })
+    }
+}
+
+fn 打印全部(内容: &String) {
+    for line in 内容.lines() {
+        if !line.is_empty() { println!("{line}"); }
+    }
+}
+
+pub fn run(配置: &输入配置) -> Result<(), Box<dyn Error>> {
+    let 内容 = fs::read_to_string(&配置.文件地址)?;
+    if 配置.打印全部吗 {
+        打印全部(&内容);
+        return Ok(());
+    }
+    let 结果 = 查找(&配置.查询字段, &内容).unwrap_or_else(
+        || {
+            eprintln!("未找到该字段");
+            process::exit(2);
+        }
+    );
+    for 行 in 结果 {
+        println!("{}", 行);
+    }
+    Ok(())
+}
+
+
+fn 查找<'a>(字段: &Option<String>, 内容: &'a String) -> Option<Vec<&'a str>> {
+    let mut v = Vec::new();
+    for 行 in 内容.lines() {
+        if 行.contains(&字段.clone().unwrap()) { v.push(行); }
+    }
+    if v.is_empty() { return None; }
+    Some(v)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn 获取配置_test() {
+    //     let 配置 = 获取配置();
+    //     assert_eq!(配置, Ok(输入配置 { 查询字段: "to".to_string(), 文件地址: "poem.txt".to_string() }));
+    // }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::{process};
+use rs_minigrep::*;
+fn main() {
+    let 配置 = 输入配置::获取配置().unwrap_or_else(
+        |err| {
+            eprintln!("获取输入错误 :{err}");
+            process::exit(1);
+        }
+    );
+    if let Err(e) = run(&配置) {
+        eprintln!("run 运行错误: {e} 文件地址:{}", 配置.文件地址);
+        process::exit(3);
+    }
+    // println!("{:?}", 配置);
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn run_test() {
+    //     let 配置 = 输入配置 { 查询字段: "to".to_string(), 文件地址: "poem.txt".to_string() , 打印全部吗: false};
+    //     assert_eq!(run(&配置).unwrap(), ());
+    // }
+}
+
