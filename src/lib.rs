446c3a7344cd5b6a52190655c83148620977000e
BO <@>
2024-01-09 15:31:57 +0800

    发布版本

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,27 @@ pub struct 输入配置 {
 }
 
 impl 输入配置 {
-    pub fn 获取配置() -> Result<输入配置, String> {
-        let 命令行参数: Vec<String> = env::args().skip(1).collect();
-        if 命令行参数.len() < 2 { return Err(format!("命令行参数过少 :{:?}", 命令行参数)); }
-        if 命令行参数.len() > 2 { return Err(format!("命令行参数过多 :{:?}", 命令行参数)); }
-        if 命令行参数[1] == "-all" { return Ok(输入配置 { 查询字段: None, 文件地址: 命令行参数[0].clone(), 打印全部吗: true }); }
-        let 查询字段 = 命令行参数[0].clone();
-        let 文件地址 = 命令行参数[1].clone();
-        Ok(输入配置 { 查询字段: Some(查询字段), 文件地址, 打印全部吗: false })
+    // pub fn 获取配置() -> Result<输入配置, String> {
+    //     let 命令行参数: Vec<String> = env::args().skip(1).collect();
+    //     if 命令行参数.len() < 2 { return Err(format!("命令行参数过少 :{:?}", 命令行参数)); }
+    //     if 命令行参数.len() > 2 { return Err(format!("命令行参数过多 :{:?}", 命令行参数)); }
+    //     if 命令行参数[1] == "-all" { return Ok(输入配置 { 查询字段: None, 文件地址: 命令行参数[0].clone(), 打印全部吗: true }); }
+    //     let 查询字段 = 命令行参数[0].clone();
+    //     let 文件地址 = 命令行参数[1].clone();
+    //     Ok(输入配置 { 查询字段: Some(查询字段), 文件地址, 打印全部吗: false })
+    // }
+    pub fn 获取配置() -> Result<输入配置, &'static str> {
+        let mut 命令行输入 = env::args();
+        命令行输入.next();
+        let 查询字段 = match 命令行输入.next() {
+            None => { return Err("未能获取查询字段"); }
+            Some(s) => { s }
+        };
+        let 文件地址 = match 命令行输入.next() {
+            None => { return Err("未能获取文件地址"); }
+            Some(s) => { s }
+        };
+        return if 文件地址 == "-all" { Ok(输入配置 { 查询字段: None, 文件地址: 查询字段, 打印全部吗: true }) } else { Ok(输入配置 { 查询字段: Some(查询字段), 文件地址, 打印全部吗: false }) }
     }
 }
 
@@ -46,12 +59,16 @@ pub fn run(配置: &输入配置) -> Result<(), Box<dyn Error>> {
 
 
 fn 查找<'a>(字段: &Option<String>, 内容: &'a String) -> Option<Vec<&'a str>> {
-    let mut v = Vec::new();
-    for 行 in 内容.lines() {
-        if 行.contains(&字段.clone().unwrap()) { v.push(行); }
-    }
-    if v.is_empty() { return None; }
-    Some(v)
+    // let mut v = Vec::new();
+    // for 行 in 内容.lines() {
+    //     if 行.contains(&字段.clone().unwrap()) { v.push(行); }
+    // }
+    // if v.is_empty() { return None; }
+    // Some(v)
+    let 字段 = 字段.clone().unwrap();
+    Some(内容.lines()
+        .filter(|行| 行.contains(&字段))
+        .collect())
 }
 
 #[cfg(test)]
